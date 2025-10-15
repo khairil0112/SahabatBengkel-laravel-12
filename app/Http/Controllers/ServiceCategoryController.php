da7a3c829286bb6998b758d2770fbd759c424936
khairil0112 <sahrulsjdn3@gmail.com>
2025-10-15 23:53:21 +0700

    create migration = database

---
+++ app/Http/Controllers/ServiceCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\serviceCategory;
+use Illuminate\Http\Request;
+
+class ServiceCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(serviceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(serviceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, serviceCategory $serviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(serviceCategory $serviceCategory)
+    {
+        //
+    }
+}
